827d5148ee366ff62f5b576624d1333a6731c139
La Legnano Informatica <info@lalegnanoinformatica.it>
2026-02-07 17:03:27 +0100

    Update db_config.php with production credentials

--- includes/db_connect.php
+++ includes/db_connect.php
@@ -1,8 +1,8 @@
 <?php
 $host = 'localhost';
-$dbname = 'elettricista_milano'; // Update with actual DB name if different
-$username = 'root';
-$password = '';
+$dbname = 'arvcl_db'; // Update with actual DB name if different
+$username = 'arvcl_us3r';
+$password = '#NXB^r6kmtx5xa8b';
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
