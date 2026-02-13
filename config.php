60413f87d8db6875a6729e3ca743629e96db963e
La Legnano Informatica <info@lalegnanoinformatica.it>
2026-02-13 20:36:37 +0100

    Force add config with correct DB and master migration script

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    'db_host' => 'localhost',
+    'db_name' => 'riparazineconsolelegnano_db',
+    'db_user' => 'riparazineconsolelegnano_us3r',
+    'db_pass' => '8e7E_qA?bewFnsk7',
+];
